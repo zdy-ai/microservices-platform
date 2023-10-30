ce6ca6fe3decec37e166c056e2639c1372fd6b00
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-10-30 16:42:37 +0800

    增加 物流平台对接

---
+++ zlt-sdk/zlt-sdk-tms-express/src/main/java/com/sdk/tms/express/ZltSdkTmsExpressApplication.java
@@ -0,0 +1,13 @@
+package com.sdk.tms.express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZltSdkTmsExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZltSdkTmsExpressApplication.class, args);
+	}
+
+}
