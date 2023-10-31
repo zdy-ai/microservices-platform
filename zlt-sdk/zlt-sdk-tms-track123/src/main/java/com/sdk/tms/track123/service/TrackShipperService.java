46475ff6e14222b43b259208422a167580218064
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-10-31 16:55:32 +0800

    增加 track123物流平台对接

---
+++ zlt-sdk/zlt-sdk-tms-track123/src/main/java/com/sdk/tms/track123/service/TrackShipperService.java
@@ -0,0 +1,21 @@
+package com.sdk.tms.track123.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zdy
+ * @ClassName TrackShipperService
+ * @description: 物流商对接服务
+ * @date 2023年10月31日
+ * @version: 1.0
+ */
+@Slf4j
+@Component
+public class TrackShipperService {
+    String host = "";
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ zlt-sdk/zlt-sdk-tms-track123/src/test/java/com/sdk/tms/track123/ZltSdkTmsTrack123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdk.tms.track123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZltSdkTmsTrack123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
