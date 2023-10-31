4e2ef3dc5ff582af5d16e236629dbb12e350c7d3
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-10-31 16:55:59 +0800

    增加 递四方物流平台对接

---
+++ zlt-sdk/zlt-sdk-tms-dsf/src/main/java/com/sdk/tms/dsf/ZltSdkTmsDsfApplication.java
@@ -0,0 +1,13 @@
+package com.sdk.tms.dsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZltSdkTmsDsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZltSdkTmsDsfApplication.class, args);
+	}
+
+}
---
+++ zlt-sdk/zlt-sdk-tms-dsf/src/test/java/com/sdk/tms/dsf/ZltSdkTmsDsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdk.tms.dsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZltSdkTmsDsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
